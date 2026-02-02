f97ac136e5403473f08f93aa0b30289c9f309b4f
sangeetashettysv <sangeetashettysv@gmail.com>
2026-02-02 19:20:39 +0100

    Code changes to remove warnings from Github

--- src/test/java/com/cgm/frontend/runner/TestRunner.java
+++ src/test/java/com/cgm/frontend/runner/TestRunner.java
@@ -4,13 +4,13 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/resources/features",
-        glue = "com.cgm.frontend.stepDefinitions",
-        plugin = {
-                "pretty",
-                "html:target/cucumber-report/cucumber.html"
-        },
-        monochrome = true
+    features = "src/test/resources/features",
+    glue = "com.cgm.frontend.stepDefinitions",
+    plugin = {
+        "pretty",
+        "html:target/cucumber-reports.html",  // generate HTML report
+        "json:target/cucumber.json"           // optional JSON report
+    },
+    monochrome = true
 )
-public class TestRunner extends AbstractTestNGCucumberTests {
-}
+public class TestRunner extends AbstractTestNGCucumberTests { }
